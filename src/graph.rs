3fe9eb279e6323ed30b27a806e891031f3f078bf
shevtsiv <rostykshevtsiv@gmail.com>
2019-03-01 21:31:24 +0200

    Basic tests coverage

--- src/graph.rs
+++ src/graph.rs
@@ -1,6 +1,6 @@
 use crate::vector_based::VectorBasedDataStructure;
 
-#[derive(PartialEq)]
+#[derive(PartialEq, Debug)]
 pub struct GraphNode<'a, T> {
     value: T,
     lines: Vec<&'a GraphNode<'a, T>>,
@@ -58,5 +58,70 @@ impl<'a, T: PartialEq> VectorBasedDataStructure<T> for Graph<'a, T> {
 
     fn capacity(&self) -> usize {
         self.nodes.capacity()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::vector_based::VectorBasedDataStructure;
+    use crate::graph::Graph;
+
+    #[test]
+    fn new() {
+        let graph: Graph<i32> = Graph::new();
+        assert_eq!(graph.len(), 0);
+        assert_eq!(graph.capacity(), 0);
+    }
+
+    #[test]
+    fn with_capacity() {
+        let graph: Graph<i32> = Graph::with_capacity(10);
+        assert_eq!(graph.len(), 0);
+        assert_eq!(graph.capacity(), 10);
+    }
+
+    use crate::graph::GraphNode;
+
+    #[test]
+    fn get_and_set_value() {
+        let mut graph_node = GraphNode::new(5);
+        assert_eq!(graph_node.get_value(), &5);
+        graph_node.set_value(10);
+        assert_eq!(graph_node.get_value(), &10);
+    }
+
+    #[test]
+    fn new_with_lines_and_get_lines() {
+        let first_node = GraphNode::new(1);
+        let second_node = GraphNode::new(2);
+        let third_node = GraphNode::new(3);
+        let lines = vec![&first_node, &second_node, &third_node];
+        let graph_node = GraphNode::new_with_lines(0, lines);
+        assert_eq!(graph_node.get_lines(), &vec![&GraphNode::new(1), &GraphNode::new(2), &GraphNode::new(3)]);
+    }
+
+    #[test]
+    fn add_line() {
+        let mut graph_node = GraphNode::new(0);
+        assert!(graph_node.get_lines().is_empty());
+        let another_graph_node = GraphNode::new(1);
+        graph_node.add_line(&another_graph_node);
+        assert_eq!(graph_node.get_lines(), &vec![&another_graph_node]);
+    }
+
+    #[test]
+    fn remove_line() {
+        let first_node = GraphNode::new(1);
+        let second_node = GraphNode::new(2);
+        let third_node = GraphNode::new(3);
+        let lines = vec![&first_node, &second_node, &third_node];
+        let mut graph_node = GraphNode::new_with_lines(0, lines);
+        assert_eq!(graph_node.get_lines(), &vec![&GraphNode::new(1), &GraphNode::new(2), &GraphNode::new(3)]);
+        graph_node.remove_line(&second_node);
+        assert_eq!(graph_node.get_lines(), &vec![&GraphNode::new(1), &GraphNode::new(3)]);
+        graph_node.remove_line(&third_node);
+        assert_eq!(graph_node.get_lines(), &vec![&GraphNode::new(1)]);
+        graph_node.remove_line(&first_node);
+        assert!(graph_node.get_lines().is_empty());
     }
 }
