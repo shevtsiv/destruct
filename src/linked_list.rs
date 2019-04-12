0a75fcf30c1c4104754a2c5d703c90c8b1701b43
shevtsiv <rostykshevtsiv@gmail.com>
2019-04-12 22:49:37 +0300

    Very raw implementation of getting a tail in LinkedList

--- src/linked_list.rs
+++ src/linked_list.rs
@@ -1,17 +1,18 @@
 use std::rc::Rc;
 
+#[derive(PartialOrd, PartialEq, Debug)]
 pub struct LinkedListNode<T> {
     data: Option<T>,
     next_node: Option<Rc<LinkedListNode<T>>>,
 }
 
 pub struct LinkedList<T> {
-    head: Option<LinkedListNode<T>>,
+    head: Option<Rc<LinkedListNode<T>>>,
 }
 
 impl<T> LinkedListNode<T> {
-    pub fn get_data(&self) -> &Option<T> {
-        &self.data
+    pub fn get_data(&self) -> Option<&T> {
+        self.data.as_ref()
     }
 
     pub fn get_data_mut(&mut self) -> Option<&mut T> {
@@ -25,15 +26,46 @@ impl<T> LinkedListNode<T> {
     pub fn get_next_mut(&mut self) -> Option<&mut Rc<LinkedListNode<T>>> {
         self.next_node.as_mut()
     }
+
+    pub fn set_next(&mut self, next: LinkedListNode<T>) {
+        self.next_node = Some(Rc::from(next));
+    }
+
+    pub fn has_next(&self) -> bool {
+        self.next_node.is_some()
+    }
 }
 
 impl<T> LinkedList<T> {
-    pub fn get_head(&self) -> Option<&LinkedListNode<T>> {
+    pub fn get_head(&self) -> Option<&Rc<LinkedListNode<T>>> {
         self.head.as_ref()
     }
 
-    pub fn get_head_mut(&mut self) -> Option<&mut LinkedListNode<T>> {
+    pub fn get_head_mut(&mut self) -> Option<&mut Rc<LinkedListNode<T>>> {
         self.head.as_mut()
+    }
+
+    pub fn get_tail(&self) -> Option<&Rc<LinkedListNode<T>>> {
+        // Empty LinkedList has no head
+        if let Some(head) = &self.head {
+            // Starting from the second node
+            let mut tail = head.next_node.as_ref();
+            // If head is a tail (LinkedList with only element)
+            if tail.is_none() {
+                return Some(head);
+            }
+            loop { // Loop until the last node is found
+                if tail.unwrap().next_node.is_none() {
+                    // It is the last node in the list
+                    return tail;
+                } else {
+                    // Move on to the next node
+                    tail = tail.unwrap().next_node.as_ref();
+                }
+            }
+        } else {
+            return None;
+        }
     }
 
     pub fn new() -> Self {
@@ -43,10 +75,50 @@ impl<T> LinkedList<T> {
 
 #[cfg(test)]
 mod tests {
-    use crate::linked_list::LinkedList;
+    use std::rc::Rc;
+    use crate::linked_list::{LinkedList, LinkedListNode};
 
     #[test]
     fn new() {
         let _linked_list: LinkedList<i32> = LinkedList::new();
+    }
+
+    #[test]
+    fn get_tail() {
+        let empty: LinkedList<i32> = LinkedList::new();
+        assert_eq!(empty.get_tail(), None);
+        let has_tail = LinkedList {
+            head: Some(Rc::from(LinkedListNode {
+                data: Some(1),
+                next_node: Some(Rc::from(LinkedListNode {
+                    data: Some(2),
+                    next_node: None,
+                })),
+            }))
+        };
+        assert_eq!(has_tail.get_tail().unwrap().get_data().unwrap(), &2);
+        let head_only = LinkedList {
+            head: Some(Rc::from(LinkedListNode { data: Some(1), next_node: None }))
+        };
+        assert_eq!(head_only.get_tail().unwrap().get_data().unwrap(), &1);
+        let deep_tail = LinkedList {
+            head: Some(Rc::from(LinkedListNode {
+                data: Some(1),
+                next_node: Some(Rc::from(LinkedListNode {
+                    data: Some(2),
+                    next_node: Some(Rc::from(LinkedListNode {
+                        data: Some(3),
+                        next_node: Some(Rc::from(LinkedListNode {
+                            data: Some(4),
+                            next_node: Some(Rc::from(LinkedListNode {
+                                data: Some(5),
+                                next_node: None,
+                            })),
+                        })),
+                    })),
+                })),
+            }))
+        };
+        assert_eq!(deep_tail.get_tail().unwrap().get_data().unwrap(), &5);
     }
 }
