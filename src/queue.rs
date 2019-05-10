5b41b48e836d8c710bd04bf65d6abb2d0ba4975b
shevtsiv <rostykshevtsiv@gmail.com>
2019-05-10 21:26:05 +0300

    Implement dequeue functionality for Queue

--- src/queue.rs
+++ src/queue.rs
@@ -19,13 +19,27 @@ impl<T> Queue<T> {
             next: None,
         }));
         if let Some(current_tail) = &mut self.tail {
-            current_tail.borrow_mut().next = Some(new_tail.clone());
+            current_tail.borrow_mut().next.replace(new_tail.clone());
             self.tail.replace(new_tail);
         } else {
             self.head.replace(new_tail.clone());
-            self.tail.replace(new_tail.clone());
+            self.tail.replace(new_tail);
         }
         self.len += 1;
+    }
+
+    pub fn dequeue(&mut self) -> Option<T> {
+        return self.head.take().map(|head| {
+            if Rc::ptr_eq(&head, self.tail.as_ref().unwrap()) {
+                self.tail = None;
+            }
+            let head = Rc::try_unwrap(head)
+                .unwrap_or_else(|_| panic!("Failed to unwrap Rc to the head of the Queue"));
+            let inner_head = head.into_inner();
+            self.head = inner_head.next.clone();
+            self.len -= 1;
+            return inner_head.data;
+        });
     }
 
     pub fn len(&self) -> usize {
@@ -69,5 +83,30 @@ mod tests {
         assert_eq!(queue.len(), 4);
         queue.enqueue(5);
         assert_eq!(queue.len(), 5);
+    }
+
+    #[test]
+    fn dequeue() {
+        let mut queue = Queue::from(vec![1, 2, 3, 4, 5]);
+        assert_eq!(queue.dequeue().unwrap(), 1);
+        assert_eq!(queue.dequeue().unwrap(), 2);
+        assert_eq!(queue.dequeue().unwrap(), 3);
+        assert_eq!(queue.dequeue().unwrap(), 4);
+        assert_eq!(queue.dequeue().unwrap(), 5);
+        assert_eq!(queue.dequeue(), None);
+    }
+
+    #[test]
+    fn enqueue_dequeue() {
+        let mut queue = Queue::new();
+        queue.enqueue(1);
+        queue.enqueue(2);
+        assert_eq!(queue.dequeue().unwrap(), 1);
+        assert_eq!(queue.dequeue().unwrap(), 2);
+        queue.enqueue(3);
+        assert_eq!(queue.dequeue().unwrap(), 3);
+        queue.enqueue(4);
+        assert_eq!(queue.dequeue().unwrap(), 4);
+        assert_eq!(queue.dequeue(), None);
     }
 }
